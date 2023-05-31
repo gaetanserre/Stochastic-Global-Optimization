17c6b3285f27bbf0f82e388a3bc0f31f56a00381
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-05-31 20:35:53 +0200

    Adam

--- optims/GO_SVGD.py
+++ optims/GO_SVGD.py
@@ -7,7 +7,7 @@ from scipy.spatial.distance import pdist, squareform
 from .__optimizer__ import Optimizer
 
 
-class AdaGrad:
+""" class AdaGrad:
     def __init__(
         self,
         lr=0.01,
@@ -22,9 +22,10 @@ class AdaGrad:
         self.initial_accumulator_value = initial_accumulator_value
         self.eps = eps
         self.state_sum = 0
-        self.t = 1
+        self.t = 0
 
     def step(self, grad, params):
+        self.t += 1
         gamma_t = self.lr / (1 + (self.t - 1) * self.lr_decay)
 
         if self.weight_decay != 0:
@@ -32,7 +33,42 @@ class AdaGrad:
 
         self.state_sum += grad**2
 
-        return gamma_t / (np.sqrt(self.state_sum) + self.eps)
+        return gamma_t / (np.sqrt(self.state_sum) + self.eps) """
+
+
+class Adam:
+    def __init__(
+        self,
+        lr=0.001,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        amsgrad=False,
+    ):
+        self.lr = lr
+        self.betas = betas
+        self.eps = eps
+        self.amsgrad = amsgrad
+        self.state_m = 0
+        self.state_v = 0
+        self.state_v_max = 0
+        self.t = 0
+
+    def step(self, grad, params):
+        self.t += 1
+
+        grad = -grad
+
+        self.state_m = self.betas[0] * self.state_m + (1 - self.betas[0]) * grad
+        self.state_v = self.betas[1] * self.state_v + (1 - self.betas[1]) * grad**2
+
+        m_hat = self.state_m / (1 - self.betas[0] ** self.t)
+        v_hat = self.state_v / (1 - self.betas[1] ** self.t)
+
+        if self.amsgrad:
+            self.state_v_max = np.maximum(self.self.state_v_max, v_hat)
+            return self.lr * m_hat / (np.sqrt(self.state_v_max) + self.eps)
+        else:
+            return params - self.lr * m_hat / (np.sqrt(v_hat) + self.eps)
 
 
 def gradient(f, x, eps=1e-12):
@@ -52,7 +88,7 @@ def rbf(x, h=-1):
     pairwise_dists = squareform(sq_dist) ** 2
     if h < 0:  # if h < 0, using median trick
         h = np.median(pairwise_dists)
-        h = np.sqrt(0.5 * h / np.log(x.shape[0] + 1))
+        h = np.sqrt(0.5 * h / np.log(x.shape[0] + 1)) + 1e-10
 
     # compute the rbf kernel
     Kxy = np.exp(-pairwise_dists / h**2 / 2)
@@ -90,11 +126,10 @@ class GO_SVGD(Optimizer):
         )
 
         for k in self.k_iter:
-            optimizer = AdaGrad(lr=0.1)
+            optimizer = Adam(lr=0.1)
             for i in range(self.svgd_iter):
                 svgd_grad = svgd(x, np.array([logprob_grad(k)(xi) for xi in x]), kernel)
-                step_size = optimizer.step(svgd_grad, x)
-                x = x + step_size * svgd_grad
+                x = optimizer.step(svgd_grad, x)
 
                 # clamp to domain
                 x = np.clip(x, self.domain[:, 0], self.domain[:, 1])
