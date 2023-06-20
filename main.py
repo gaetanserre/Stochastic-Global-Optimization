f2eedacf77cbffc263f41e3d89c00128b7c7dbb8
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2023-06-20 11:04:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def run_exps(
                 bounds,
                 n_particles=10,
                 k_iter=[100_000],
-                svgd_iter=10 if is_simu else 500,
+                svgd_iter=30 if is_simu else 500,
                 lr=0.1 if is_simu else 0.7,
             )
         else:
