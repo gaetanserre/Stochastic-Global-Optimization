7e40acfc477e9a8a716651de79ab2d8f64c8b60f
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-07-10 17:32:55 +0200

    Color lambda funs

--- main.py
+++ main.py
@@ -30,22 +30,45 @@ def time_it(function, args={}):
     return ret, end - start
 
 
-def print_yellow(str):
-    print("\033[93m" + str + "\033[0m")
+def print_color(str, color):
+    print(f"\033[{color}m" + str + "\033[0m")
 
 
-def print_blue(str):
-    print("\033[94m" + str + "\033[0m")
+print_yellow = lambda str: print_color(str, 93)
+print_blue = lambda str: print_color(str, 94)
+print_green = lambda str: print_color(str, 92)
 
 
-def print_green(str):
-    print("\033[92m" + str + "\033[0m")
+def match_optim(optim_cls, bounds, num_evals, is_sim=False):
+    if optim_cls == PRS:
+        return optim_cls(bounds, num_evals=num_evals)
+    elif optim_cls == AdaLIPO_E:
+        return optim_cls(bounds, max_evals=num_evals)
+    elif optim_cls == CMA_ES:
+        m_0 = np.random.uniform(bounds[:, 0], bounds[:, 1])
+        return optim_cls(
+            bounds,
+            m_0,
+            num_generations=num_evals // 5,
+            lambda_=5,
+            cov_method="full",
+        )
+    elif optim_cls == GO_SVGD:
+        return optim_cls(
+            bounds,
+            n_particles=10,
+            k_iter=[100_000],
+            svgd_iter=500,
+            lr=0.5 if is_sim else 0.1,
+        )
+    else:
+        raise NotImplementedError(f"{optim_cls} not implemented.")
 
 
 if __name__ == "__main__":
     num_exp = 5
 
-    functions = [Rosenbrock()]
+    functions = [Square(), Rosenbrock()]
 
     bounds = [
         np.array(
@@ -71,41 +94,48 @@ if __name__ == "__main__":
                 (-10, 10),
                 (-10, 10),
             ]
-        )
+        ),
+        np.array(
+            [
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+                (-10, 10),
+            ]
+        ),
     ]
 
-    optimizers_cls = [PRS, CMA_ES, GO_SVGD]
+    optimizers_cls = [PRS, AdaLIPO_E, CMA_ES, GO_SVGD]
 
-    num_eval = 50000
+    num_eval = 1500
 
     for i, function in enumerate(functions):
         print_yellow(f"Function: {function.__class__.__name__}.")
         for optimizer_cls in optimizers_cls:
             print_blue(f"Optimizer: {optimizer_cls.__name__}.")
 
-            if optimizer_cls == PRS:
-                optimizer = optimizer_cls(bounds[i], num_evals=num_eval)
-            elif optimizer_cls == AdaLIPO_E:
-                optimizer = optimizer_cls(bounds[i], max_evals=num_eval)
-            elif optimizer_cls == CMA_ES:
-                m_0 = np.random.uniform(bounds[i][:, 0], bounds[i][:, 1])
-                optimizer = optimizer_cls(
-                    bounds[i],
-                    m_0,
-                    num_generations=num_eval // 5,
-                    lambda_=5,
-                    cov_method="full",
-                )
-            elif optimizer_cls == GO_SVGD:
-                optimizer = optimizer_cls(
-                    bounds[i],
-                    n_particles=10,
-                    k_iter=[100_000],
-                    svgd_iter=500,
-                    lr=0.1 if function.__class__.__name__ == "Simulation" else 0.5,
-                )
-            else:
-                raise NotImplementedError(f"{optimizer_cls} not implemented.")
+            optimizer = match_optim(
+                optimizer_cls,
+                bounds[i],
+                num_eval,
+                is_sim=function.__class__.__name__ == "Simulation",
+            )
 
             times = []
             best_values = []
