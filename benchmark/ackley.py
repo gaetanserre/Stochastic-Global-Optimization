cc9fcabf0d50643f0e2535b39363c23b8ee34767
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-08-10 12:33:38 +0200

    Ackley function

---
+++ benchmark/ackley.py
@@ -0,0 +1,24 @@
+#
+# Created in 2023 by Gaëtan Serré
+#
+
+import numpy as np
+from .__function__ import Function
+
+
+class Ackley(Function):
+    def __init__(self) -> None:
+        super().__init__()
+        self.n = 0
+
+    def __call__(self, x: np.ndarray) -> float:
+        self.n += 1
+        a = 20
+        b = 0.2
+        c = 2 * np.pi
+        return -(
+            -a * np.exp(-b * np.sqrt(np.sum(x**2) / len(x)))
+            - np.exp(np.sum(np.cos(c * x)) / len(x))
+            + a
+            + np.exp(1)
+        )
