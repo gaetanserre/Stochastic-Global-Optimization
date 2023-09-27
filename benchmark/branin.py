fa3f748935c885bac8e860699c9e1e482da02902
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2023-09-27 17:41:13 +0200

    Branin function

---
+++ benchmark/branin.py
@@ -0,0 +1,26 @@
+#
+# Created in 2023 by Gaëtan Serré
+#
+
+import numpy as np
+from .__function__ import Function
+
+
+class Branin(Function):
+    def __init__(self) -> None:
+        super().__init__()
+        self.n = 0
+
+    def __call__(self, x: np.ndarray) -> float:
+        self.n += 1
+        a = 1
+        b = 5.1 / (4 * np.pi) ** 2
+        c = 5 / np.pi
+        r = 6
+        s = 10
+        t = 1 / (8 * np.pi)
+        return (
+            a * (x[1] - b * x[0] ** 2 + c * x[0] - r) ** 2
+            + s * (1 - t) * np.cos(x[0])
+            + s
+        )
