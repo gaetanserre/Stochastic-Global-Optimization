d06aa8b9b04fcd7c5c9f2b6eaabf0cca997d7b09
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-01-18 14:57:14 +0100

    sigma

--- optims/SBS.py
+++ optims/SBS.py
@@ -54,19 +54,19 @@ def gradient(f, x, eps=1e-12):
     return grad
 
 
-def rbf(x, h=-1):
+def rbf(x, sigma=-1):
     sq_dist = pdist(x)
     pairwise_dists = squareform(sq_dist) ** 2
-    if h < 0:  # if h < 0, using median trick
-        h = np.median(pairwise_dists) + 1e-10
-        h = np.sqrt(0.5 * h / np.log(x.shape[0] + 1))
+    if sigma < 0:  # if sigma < 0, using median trick
+        sigma = np.median(pairwise_dists) + 1e-10
+        sigma = np.sqrt(0.5 * sigma / np.log(x.shape[0] + 1))
 
     # compute the rbf kernel
-    Kxy = np.exp(-pairwise_dists / h**2 / 2)
+    Kxy = np.exp(-pairwise_dists / sigma**2 / 2)
 
     dxkxy = (x * Kxy.sum(axis=1).reshape(-1, 1) - Kxy @ x).reshape(
         x.shape[0], x.shape[1]
-    ) / (h**2)
+    ) / (sigma**2)
 
     return Kxy, dxkxy
 
@@ -79,17 +79,18 @@ def svgd(x, logprob_grad, kernel):
 
 
 class SBS(Optimizer):
-    def __init__(self, domain, n_particles, k_iter, svgd_iter, lr=0.5):
+    def __init__(self, domain, n_particles, k_iter, svgd_iter, sigma=-1, lr=0.5):
         self.domain = domain
         self.n_particles = n_particles
         self.k_iter = k_iter
         self.svgd_iter = svgd_iter
+        self.sigma = sigma
         self.lr = lr
 
     def optimize(self, function, verbose=False):
         logprob_grad = lambda k: (lambda x: -k * gradient(function, x))
 
-        kernel = rbf
+        kernel = lambda x: rbf(x, sigma=self.sigma)
 
         dim = self.domain.shape[0]
 
