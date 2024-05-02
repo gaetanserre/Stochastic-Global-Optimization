12441d45517c666223148dda74e8d29aa88c500c
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-05-02 16:26:21 +0200

    MALA

--- main_para.py
+++ main_para.py
@@ -94,9 +94,7 @@ def match_optim(optim_cls, bounds, num_evals, is_sim=False):
     elif optim_cls == SimulatedAnnealing:
         return optim_cls(bounds, num_evals)
     elif optim_cls == Langevin:
-        return optim_cls(
-            bounds, n_iter=num_evals // (len(bounds) + 1), kappa=10_000, init_lr=0.2
-        )
+        return optim_cls(bounds, n_particles=500, n_iter=300, kappa=10_000, init_lr=0.2)
     else:
         raise NotImplementedError(f"{optim_cls} not implemented.")
 
