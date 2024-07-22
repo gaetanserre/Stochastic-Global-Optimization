f5ef221d9f15d6f75a4a3aa269ff4d55c446b953
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-07-22 17:20:59 +0200

    print_green

--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ def print_color(str, color):
 
 print_pink = lambda str: print_color(str, 95)
 print_blue = lambda str: print_color(str, 94)
-print_green = lambda str: print_color(str, 92)
+print_green = lambda str: print_color(str, 32)
 print_purple = lambda str: print_color(str, 35)
 
 
